ac4ba7e4375b08c5d7a581cd8655591c9fd78b71
Vladimir Rubin <vavakado@proton.me>
2024-07-07 01:38:37 +0300

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,7 +7,27 @@ if status is-interactive
 	alias la="eza --icons --all"
 	alias ll="eza --icons --long"
 
-	export PATH="$HOME/.node_modules_global/bin:$PATH"
+	# xdg-ninja
+	export XDG_DATA_HOME="$HOME/.local/share"
+	export XDG_CONFIG_HOME="$HOME/.config"
+	export XDG_STATE_HOME="$HOME/.local/state"
+	export XDG_CACHE_HOME="$HOME/.cache"
+	export W3M_DIR="$XDG_DATA_HOME"/w3m
+	export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+	export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+	export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
+	export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
+	export TERMINFO="$XDG_DATA_HOME"/terminfo
+	export TERMINFO_DIRS="$XDG_DATA_HOME"/terminfo:/usr/share/terminfo
+	export NBRC_PATH="$XDG_CONFIG_HOME/nbrc"
+	export NB_DIR="$XDG_DATA_HOME/nb"
+	export ICEAUTHORITY="$XDG_CACHE_HOME"/ICEauthority
+	export LESSHISTFILE="$XDG_STATE_HOME"/less/history
+	export XCURSOR_PATH=/usr/share/icons:$XDG_DATA_HOME/icons
+	export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+	export GOPATH="$XDG_DATA_HOME"/go
+	export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+
 	fish_add_path -p ~/.cargo/bin ~/.local/bin/
 	export EDITOR="nvim"
 
