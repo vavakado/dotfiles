1112e31a8f3c7ea0d5c08c14ad02c6ae8a43458f
Vladimir Rubin <vavakado@proton.me>
2024-11-01 02:28:53 +0200

    hell yeah

---
+++ dot_config/hypr/scripts/executable_wallch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Default directory
+default_dir="${HOME}/.config/wallpapers/"
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    # No argument, use default directory
+    dir="$default_dir"
+else
+    # Argument provided, use it as the directory
+    dir="$1"
+fi
+
+BG="$(find "$dir" -maxdepth 1 -name '*.jpg' -o -name '*.png' | shuf -n1)"
+
+#trans_type="wipe"
+trans_type="fade"
+swww img "$BG" --transition-fps 75 --transition-type $trans_type --transition-duration 2.5 --resize crop
